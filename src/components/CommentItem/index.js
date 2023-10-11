5907dc12c66c49a8a4ba48e76e4eb8d897958648
Bhargav-9121 <bkc.edu9121@gmail.com>
2023-10-11 05:25:46 +0000

    css not done yet

---
+++ src/components/CommentItem/index.js
@@ -0,0 +1,39 @@
+import {formatDistanceToNow} from 'date-fns'
+
+const Listing = props => {
+  const {val, togLike, delThing} = props
+  const {id, name, para, isLiked, time} = val
+  const postedTime = formatDistanceToNow(time)
+  const dp = name[0].toUpperCase()
+  const imgUrl = isLiked
+    ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+
+  const doLike = () => {
+    togLike(id)
+  }
+
+  const doDel = () => {
+    delThing(id)
+  }
+
+  return (
+    <li>
+      <button type="button">{dp}</button>
+      <p>{postedTime}</p>
+      <p>{name}</p>
+      <p>{para}</p>
+      <button onClick={doLike} type="button">
+        <img src={imgUrl} alt="likeThing" />
+      </button>
+      <button onClick={doDel} data-testid="delete" type="button">
+        <img
+          alt="delete"
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default Listing
---
+++ src/components/Comments/index.js
@@ -0,0 +1,120 @@
+import {Component} from 'react'
+import './index.css'
+import {v4 as uuidv4} from 'uuid'
+import Listing from '../CommentItem'
+
+const initialContainerBackgroundClassNames = [
+  'amber',
+  'blue',
+  'orange',
+  'emerald',
+  'teal',
+  'red',
+  'light-blue',
+]
+
+class Comments extends Component {
+  state = {
+    commentsList: [],
+    nameInput: '',
+    paraInput: '',
+  }
+
+  nameThing = event => {
+    this.setState({nameInput: event.target.value})
+  }
+
+  paraThing = event => {
+    this.setState({paraInput: event.target.value})
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+
+    const {nameInput, paraInput} = this.state
+
+    const cla =
+      initialContainerBackgroundClassNames[
+        Math.ceil(
+          Math.random() * initialContainerBackgroundClassNames.length - 1,
+        )
+      ]
+
+    const newComment = {
+      id: uuidv4(),
+      name: nameInput,
+      para: paraInput,
+      isLiked: false,
+      dpClass: cla,
+      time: new Date(),
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      nameInput: '',
+      paraInput: '',
+    }))
+  }
+
+  togLike = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachC => {
+        if (id === eachC.id) {
+          return {...eachC, isLiked: !eachC.isLiked}
+        }
+        return eachC
+      }),
+    }))
+  }
+
+  delThing = id => {
+    const {commentsList} = this.state
+    this.setState({commentsList: commentsList.filter(each => each.id !== id)})
+  }
+
+  render() {
+    const {commentsList, nameInput, paraInput} = this.state
+    return (
+      <>
+        <form onSubmit={this.submitForm}>
+          <div className="written-div">
+            <h1>Comments</h1>
+            <p>Say Something about 4.0 Technologies</p>
+            <input
+              value={nameInput}
+              onChange={this.nameThing}
+              placeholder="Your Name"
+            />
+            <textarea
+              value={paraInput}
+              onChange={this.paraThing}
+              placeholder="Your Comment"
+              rows="4"
+              cols="30"
+            />
+            <button type="submit">Add Comment</button>
+          </div>
+          <img
+            alt="comments"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+          />
+        </form>
+        <hr />
+        <p>
+          <span>{commentsList.length}</span> Comments
+        </p>
+        <ul>
+          {commentsList.map(each => (
+            <Listing
+              delThing={this.delThing}
+              togLike={this.togLike}
+              key={each.id}
+              val={each}
+            />
+          ))}
+        </ul>
+      </>
+    )
+  }
+}
+
+export default Comments
